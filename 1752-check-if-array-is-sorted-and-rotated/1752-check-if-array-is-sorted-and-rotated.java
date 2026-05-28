64d9fa9bc5d9e6a5524d768fd0a57794d36b61e8
Naare Sudharshan Kumar <dharshankumar169@gmail.com>
2026-05-28 07:18:58 +0530

    Time: 0 ms (100%), Space: 43.3 MB (10.88%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -1,37 +1,13 @@
 class Solution {
-
     public boolean check(int[] nums) {
+        return check(0,0, nums);
+    }
+    private boolean check (int i ,int cnt,  int[] nums){
         int n = nums.length;
-
-        // Construct the rotated array
-        int[] checkSorted = new int[n];
-
-        // Iterate through all possible rotation offsets
-        for (int rotationOffset = 0; rotationOffset < n; ++rotationOffset) {
-            int currIndex = 0;
-            for (int index = rotationOffset; index < n; ++index) {
-                checkSorted[currIndex++] = nums[index];
-            }
-            for (int index = 0; index < rotationOffset; ++index) {
-                checkSorted[currIndex++] = nums[index];
-            }
-
-            // Check if the constructed array is sorted
-            boolean isSorted = true;
-            for (int index = 0; index < n - 1; ++index) {
-                if (checkSorted[index] > checkSorted[index + 1]) {
-                    isSorted = false;
-                    break;
-                }
-            }
-
-            // If sorted, return true
-            if (isSorted) {
-                return true;
-            }
-        }
-
-        // If no rotation makes the array sorted, return false
-        return false;
+        if(i == n) return cnt <=1;
+        int next = (i+1) % n;
+        if(nums[i] > nums[next]) cnt++;
+        if(cnt >1) return false;
+        return check(i+1,cnt, nums);
     }
 }
