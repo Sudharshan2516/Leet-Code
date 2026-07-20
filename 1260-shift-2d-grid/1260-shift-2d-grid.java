7a40799b642736ba0fdae3b83656d21943945688
Naare Sudharshan Kumar <dharshankumar169@gmail.com>
2026-07-20 10:34:01 +0530

    Time: 89 ms (5.75%), Space: 47.2 MB (64.37%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.java
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -1,35 +1,44 @@
 class Solution {
     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
-        int m = grid.length, n = grid[0].length;
-        if(k == 0) return Arrays.stream(grid)
-            .map(row -> Arrays.stream(row)
-                              .boxed()
-                              .collect(Collectors.toList()))
-            .collect(Collectors.toList());
-        int[][] ans = new int[m][n];
-        for(int t = 0; t < k; t++){
-            ans[0][0] = grid[m-1][n-1];
-            for(int i = 0; i< m; i++){
-                for(int j = 0; j < n; j++){
-                    if(i == m-1 && j == n-1){
-                        break;
-                    }
-                    else if(j == n-1){
-                        ans[i+1][0] = grid[i][j];
-                    }
-                    else{
-                        ans[i][j+1] = grid[i][j];
+        int m = grid.length;
+        int n = grid[0].length;
+
+        List<List<Integer>> currentGrid = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                row.add(grid[i][j]);
+            }
+            currentGrid.add(row);
+        }
+
+        for (int t = 0; t < k; t++) {
+            List<List<Integer>> ans = new ArrayList<>();
+            for (int i = 0; i < m; i++) {
+                List<Integer> row = new ArrayList<>();
+                for (int j = 0; j < n; j++) {
+                    row.add(0); 
+                }
+                ans.add(row);
+            }
+
+            ans.get(0).set(0, currentGrid.get(m - 1).get(n - 1));
+
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (i == m - 1 && j == n - 1) {
+                        continue; 
+                    } else if (j == n - 1) {
+                        ans.get(i + 1).set(0, currentGrid.get(i).get(j));
+                    } else {
+                        ans.get(i).set(j + 1, currentGrid.get(i).get(j));
                     }
                 }
             }
-              IntStream.range(0, ans.length).forEach(i -> 
-            System.arraycopy(ans[i], 0, grid[i], 0, ans[i].length)
-        );
+
+            currentGrid = ans;
         }
-        return Arrays.stream(ans)
-            .map(row -> Arrays.stream(row)
-                              .boxed()
-                              .collect(Collectors.toList()))
-            .collect(Collectors.toList());
+
+        return currentGrid;
     }
 }
