56b1365ef0f5e696afba66a9fdeee605d1746546
Naare Sudharshan Kumar <dharshankumar169@gmail.com>
2026-08-12 06:33:16 +0530

    Time: 948 ms (42.59%), Space: 0B (100%) - LeetHub

---
+++ 1407-top-travellers/1407-top-travellers-08-12-2026-06-33-14.sql
@@ -0,0 +1,8 @@
+select 
+    u.name, 
+    COALESCE(SUM(r.distance),0) as travelled_distance
+from users  u
+left join  Rides as r  on u.id = r.user_id 
+group by u.id, u.name
+order by travelled_distance desc,
+        u.name asc;
